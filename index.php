50551c7b57728c3cb8764529ba1c3af4afebd7ac
SangesAntonio <sanges65@gmail.com>
2022-02-25 16:12:58 +0100

    added snack 1

--- index.php
+++ index.php
@@ -1,19 +1,44 @@
-Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+<?php
+/*Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”*/
 
-<ph? ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
+$name = $_GET ? $_GET['name'] : '';
+$mail = $_GET ? $_GET['mail'] : '';
+$age = $_GET ? $_GET['age'] : '';
 
-    <body>
+$nameAccess = false;
+$mailAccess = false;
+$ageAccess = false;
+if (strlen($name) > 3) {
+    $nameAccess = true;
+}
+if (is_numeric($mail) && str_contains($mail, '.') && str_contains($mail, '@')) {
+    $mailAccess = true;
+}
+if (is_numeric($age)) {
+    $ageAccess = true;
+}
 
-    </body>
+var_dump($name, $mail, $age);
+var_dump($nameAccess, $mailAccess, $ageAccess);
+$message = 'Accesso negato';
+if ($nameAccess && $mailAccess && $ageAccess) $message = 'Accesso riuscito';
+?>
 
-    </html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo $message ?></h1>
+
+</body>
+
+</html>
